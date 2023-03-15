16564047a4d459b3bbec844f875c82f08425f806
Lucas Ikeda <li@moray.ai>
2023-03-14 21:38:10 -0300

    Filtering out of boundaries entries

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -32,6 +32,9 @@ def go(args):
     df = df[idx].copy()
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
+    # removing out of boundaries entries
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     # Saving to output file
     df.to_csv(args.output_artifact, index=False)
 
